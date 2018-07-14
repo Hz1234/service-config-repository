6f2b3ba394cad01eae7a02f6c8a3e0445ebba6c5
Hz <pojo@foxmail.com>
2018-07-15 00:11:26 +0800

    Initial commit

---
+++ src/main/java/com/learnbind/eureka/ServiceConfigRepositoryApplication.java
@@ -0,0 +1,12 @@
+package com.learnbind.eureka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServiceConfigRepositoryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServiceConfigRepositoryApplication.class, args);
+	}
+}
